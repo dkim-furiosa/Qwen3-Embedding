de9cc60642bd1337a8be836612eed537d8a1bc97
Deokhwan Kim <dkim@furiosa.ai>
2025-12-17 14:07:30 +0000

    Only run tasks that finish quickly

--- evaluation/run_mteb.sh
+++ evaluation/run_mteb.sh
@@ -12,5 +12,4 @@ python run_mteb.py \
   --model_kwargs "{\"max_length\": 8192, \"attn_type\": \"causal\", \"pooler_type\": \"last\", \"do_norm\": true, \"use_instruction\": true, \"instruction_template\": \"Instruct: {}\nQuery:\", \"instruction_dict_path\": \"task_prompts.json\", \"attn_implementation\":\"flash_attention_2\", \"dtype\": \"bfloat16\", \"device\": \"cpu\"}" \
   --run_kwargs "{\"save_predictions\": \"true\"}" \
   --output_dir results/${model_name} \
-  --batch_size 8 \
-  --tasks "TwentyNewsgroupsClustering.v2,TweetSentimentExtractionClassification,SCIDOCS,TwitterSemEval2015,STS22.v2,SummEvalSummarization.v2,MindSmallReranking"
+  --batch_size 8
